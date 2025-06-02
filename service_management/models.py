029c130e31aa9c23c319a7e2c671e39fe28c41ef
uwi001_1 <022a106@sxc.edu.np>
2025-06-02 21:18:59 +0545

    no

--- service_management/models.py
+++ service_management/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+import uuid
+from django.utils.text import slugify
 
 # Create your models here.
 class Service_management(models.Model):
     Name = models.CharField(max_length=255)
+    public_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    slug = models.SlugField(max_length=255, unique=True)
+
     # Category = models.ForeignKey
     Price = models.PositiveIntegerField()
     Description = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = f'{slugify(self.name)}-{str(self.public_id)[1:5]}{str(self.public_id)[-1:-5]}'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.Name
